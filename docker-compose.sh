6c3de130ecf020f0bb67279edcc6d3b77fb60216
0x42 <bpozdnyakov@gmail.com>
2021-12-07 14:53:07 +0300

    del legacy payinst field

--- docker-compose.sh
+++ docker-compose.sh
@@ -18,7 +18,7 @@ services:
     mem_limit: 512M
 
   dominant:
-    image: dr2.rbkmoney.com/rbkmoney/dominant:753f3e0711fc7fff91abcad6e279225a7e5b8b8c
+    image: dr2.rbkmoney.com/rbkmoney/dominant:bba85997674a2abca3e8d4e3638e0100177d3c13
     command: /opt/dominant/bin/dominant foreground
     depends_on:
       machinegun:
